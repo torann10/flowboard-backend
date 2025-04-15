8d9c86d205c05e2cafbd93793ca38e5635417b86
Török Anna <h270316@stud.u-szeged.hu>
2025-04-15 16:21:43 +0200

    Initial commit

---
+++ src/main/java/szte/flowboard/FlowBoardApplication.java
@@ -0,0 +1,13 @@
+package szte.flowboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/szte/flowboard/FlowBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package szte.flowboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
